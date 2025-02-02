0d204b754d377615c9144c6aa11eb96e5252c09d
kasungunasekara <162046889+Kasun4527@users.noreply.github.com>
2025-02-02 14:13:33 +0530

    first commit

---
+++ DemoApp/src/test/java/com/Kasun/DemoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kasun.DemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
